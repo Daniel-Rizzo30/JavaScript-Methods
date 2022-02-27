7fa3ba3a60edbb66312cf34de79d7a5a4a6bcb58
Daniel.Rizzo30 <rimshot816@DESKTOP-2UTBUI6.localdomain>
2022-02-26 20:28:59 -0500

    Added starter files to the repo

---
+++ assignment-2.js
@@ -0,0 +1,49 @@
+// FOR EACH //
+Array.prototype.myEach = function() {
+
+};
+
+// MAP //
+Array.prototype.myMap = function() {
+
+};
+
+// SOME //
+Array.prototype.mySome = function() {
+
+};
+
+// REDUCE //
+Array.prototype.myReduce = function() {
+
+};
+
+// INCLUDES //
+Array.prototype.myIncludes = function() {
+
+};
+
+// INDEXOF //
+Array.prototype.myIndexOf = function() {
+
+};
+
+// PUSH //
+Array.prototype.myPush = function() {
+
+};
+
+// LASTINDEXOF //
+Array.prototype.myLastIndexOf = function() {
+
+};
+
+// KEYS //
+Object.myKeys = function() {
+
+};
+
+// VALUES //
+Object.myValues = function() {
+
+};
