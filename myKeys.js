8bdbcc19ac9ac4d734b6fb8f7132470b409dca18
Daniel.Rizzo30 <rimshot816@DESKTOP-2UTBUI6.localdomain>
2022-02-27 20:38:44 -0500

    Just added myKeys file - still working on myKeys function. Not done yet

---
+++ myKeys.js
@@ -0,0 +1,46 @@
+// KEYS //
+Object.myKeys = function(obj) {
+    let attr_list = []; // Set up empty array
+    for (let attr of obj) { // for each attribute in the object ---- Doesn't work !!!!!!!!
+        attr_list.push(attr); // Push to the list
+    }
+    return attr_list; // Return the full list
+};
+
+// The Object.keys() method returns an array of a given 
+// object's own enumerable property names, iterated in the 
+// same order that a normal loop would.
+
+// TEST //
+// Test myKeys against the native keys to ensure that it works
+let myArray = [0,1,2,"snake",4,5,"cat","Scarlet Witch",2]; // Array called by the function
+// Should just list 0 - 8 when used in keys function
+
+const anObj = { 100: 'a', 2: 'b', 7: 'c' }; // Object called by the function
+// Should list 2, 7, 100 when used in keys function
+
+const myObj = Object.create({}, {
+    getFoo: {
+      value: function () { return this.foo; }
+    }
+  });
+myObj.foo = 1; 
+// Should just list foo when called in keys function
+
+// Test with myArray
+console.log("myKeys (1 parameter): object");
+console.log(Object.myKeys(myArray)); // Parameter: myArray
+console.log("keys (1 parameter): object");
+console.log(Object.keys(myArray)); // Parameter: myArray
+
+// Test with anObj
+console.log("myKeys (1 parameter): object");
+console.log(Object.myKeys(anObj)); // Parameter: anObj
+console.log("keys (1 parameter): object");
+console.log(Object.keys(anObj)); // Parameter: anObj
+
+// Test with myObj
+console.log("myKeys (1 parameter): object");
+console.log(Object.myKeys(myObj)); // Parameter: myObj
+console.log("keys (1 parameter): object");
+console.log(Object.keys(myObj)); // Parameter: myObj
